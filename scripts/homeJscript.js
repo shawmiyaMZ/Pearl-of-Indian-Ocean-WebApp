93a000b3693200b920a41368a4584605fd73f825
shawmiyaMZ <114656363+shawmiyaMZ@users.noreply.github.com>
2023-12-14 00:14:38 +0530

    Add files via upload

---
+++ scripts/homeJscript.js
@@ -0,0 +1,21 @@
+
+let formBtn = document.querySelector('#login-btn');
+let loginForm = document.querySelector('.login-form-container');
+let formClose = document.querySelector('#form-close');
+
+
+window.onscroll = () =>{
+    
+    loginForm.classList.remove('active');
+}
+
+
+
+formBtn.addEventListener('click', () =>{
+    loginForm.classList.add('active');
+});
+
+formClose.addEventListener('click', () =>{
+    loginForm.classList.remove('active');
+});
+
---
+++ scripts/tourist information scripts.js
@@ -0,0 +1,120 @@
+function autocomplete(inp, arr) {
+    
+    var currentFocus;
+ 
+    inp.addEventListener("input", function(e) {
+        var a, b, i, val = this.value;
+       
+        closeAllLists();
+        if (!val) { return false;}
+        currentFocus = -1;
+        
+        a = document.createElement("DIV");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+       
+        this.parentNode.appendChild(a);
+       
+        for (i = 0; i < arr.length; i++) {
+          
+          if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+            
+            b = document.createElement("DIV");
+           
+            b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+            b.innerHTML += arr[i].substr(val.length);
+            
+            b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+            
+            b.addEventListener("click", function(e) {
+                
+                inp.value = this.getElementsByTagName("input")[0].value;
+               
+                closeAllLists();
+            });
+            a.appendChild(b);
+          }
+        }
+    });
+    
+    inp.addEventListener("keydown", function(e) {
+        var x = document.getElementById(this.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+        if (e.keyCode == 40) {
+         
+          currentFocus++;
+          
+          addActive(x);
+        } else if (e.keyCode == 38) { //up
+         
+          currentFocus--;
+          
+          addActive(x);
+        } else if (e.keyCode == 13) {
+          
+          e.preventDefault();
+          if (currentFocus > -1) {
+            
+            if (x) x[currentFocus].click();
+          }
+        }
+    });
+    function addActive(x) {
+      /*a function to classify an item as "active":*/
+      if (!x) return false;
+      /*start by removing the "active" class on all items:*/
+      removeActive(x);
+      if (currentFocus >= x.length) currentFocus = 0;
+      if (currentFocus < 0) currentFocus = (x.length - 1);
+      /*add class "autocomplete-active":*/
+      x[currentFocus].classList.add("autocomplete-active");
+    }
+    function removeActive(x) {
+      /*a function to remove the "active" class from all autocomplete items:*/
+      for (var i = 0; i < x.length; i++) {
+        x[i].classList.remove("autocomplete-active");
+      }
+    }
+    function closeAllLists(elmnt) {
+      /*close all autocomplete lists in the document,
+      except the one passed as an argument:*/
+      var x = document.getElementsByClassName("autocomplete-items");
+      for (var i = 0; i < x.length; i++) {
+        if (elmnt != x[i] && elmnt != inp) {
+          x[i].parentNode.removeChild(x[i]);
+        }
+      }
+    }
+    /*execute a function when someone clicks in the document:*/
+    document.addEventListener("click", function (e) {
+        closeAllLists(e.target);
+    });
+  }
+  
+  /*An array containing all the country names in the world:*/
+  var disandprov = ["Ampara","Anuradhapura","Badulla","Batticaloa","Colombo","Galle","Gampaha","Hambantota","Jaffna","Kalutara","Kandy","Kegalle","Kilinochchi","Kurunegala","Mannar","Matale","Matara","Moneragala","Mullaitivu","Nuwara Eliya","Polonnaruwa","Puttalam","Ratnapura","Trincomalee","Vavuniya","Northern prov","North Western prov","Western prov","North Central prov","Central prov","Sabaragamuwa prov","Eastern prov","Uva prov","Southern prov"];
+  
+  /*initiate the autocomplete function on the "myInput" element, and pass along the countries array as possible autocomplete values:*/
+  autocomplete(document.getElementById("myInput"), disandprov);
+
+
+
+  var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
